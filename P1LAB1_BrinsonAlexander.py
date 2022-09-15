54f3c1a3b8456a3985e45924aba5a041d261841b
Brinsona5662 <113639192+Brinsona5662@users.noreply.github.com>
2022-09-15 17:02:24 -0400

    P1LAB1 Assignment

---
+++ P1LAB1_BrinsonAlexander.py
@@ -0,0 +1,10 @@
+# this program asks for the user's name, then prints a welcome message
+# 9/15/2022
+# P1LAB1-Using ZyBooks Lab Basics
+# Alexander Brinson
+
+user_name = input()
+# this asks the user for their name
+
+print('Hello', user_name, 'and welcome to CS Online!')
+# this prints the welcome message using the inputted name
